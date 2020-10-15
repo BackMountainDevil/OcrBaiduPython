100e9d7a0302ae15e0d0c8233ea217f43ed07d38
Kearney <191615342@qq.com>
2020-10-15 21:01:36 +0800

    update imgocr.py. 1.  支持识别的文本保存到文件中。 2.  增加常规的异常处理。

--- imgocr.py
+++ imgocr.py
@@ -9,6 +9,14 @@ SECRET_KEY = '你的 Secret Key'
 """ 待识别图片的路径 """
 filepath = 'tmp.PNG'
 
+# 是否保存结果到文件中，True为保存，False为不保存，直接输出
+isFileSave = True
+# 若保存，保存路径为
+saveFilePath = 'OcrText.txt'
+# 是否保留上次文件中的内容，True为保存之前的内容，并在之后添加本次内容
+# False为删除上次的内容，只保留本次识别结果
+isLastSave = True
+
 """ 读取图片的编码 """
 def get_file_content(filePath):
     with open(filePath, 'rb') as fp:
@@ -21,12 +29,28 @@ def get_text_concat(resdict):
         strx+=tex["words"]
     return(strx)
 
-if __name__ == "__main__":
-    client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
-    
-    image = get_file_content(filepath)
+""" 将数据data保存到filePath中 """ 
+def save_text(filePath, data, isLastSave = True):
+    if(isLastSave):
+        with open(filePath, 'a', encoding='utf-8') as f:
+            f.write('\n'+ data)
+    else:
+        with open(filePath, 'w', encoding='utf-8') as f:
+            f.write(data)
 
+if __name__ == "__main__":
+    print("OCR识别开始")
     """ 调用通用文字识别, 图片参数为本地图片 """
-    res = client.basicGeneral(image);
-    text = get_text_concat(res)
-    print( text)
+    try:
+        client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+        image = get_file_content(filepath)
+        res = client.basicGeneral(image)
+        text = get_text_concat(res)
+        if(isFileSave):
+            save_text(saveFilePath, text, isLastSave)
+            print("识别文字已保存到文件中")
+        else:
+            print( text)
+        print("OCR识别成功，程序结束")
+    except Exception as e:
+        print("OCR识别失败！！！程序非正常结束\n详情：",e)
